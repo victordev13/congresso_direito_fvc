ef443a7b01e518a9a7a274abe2143b06030f6d58
Análise e Desenvolvimento de Sistemas <alunos.ads@ivclabs.local>
2020-03-05 21:22:32 -0300

    update

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscrição</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="css/bootstrap.reboot.min.css"> 
+    <script src="js/functions.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.mask.min.js"></script>
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/pooper.min.js"></script>
 </head>
 <body>
     
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
     require_once 'includes/header.php';
 ?>
+<button type="button" class="btn btn-default btn-fvc">Primary</button>
 
 
 <?php 
