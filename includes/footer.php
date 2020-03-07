633efdc252e3ea30df370bdf8dade67f2b23b5d5
victordev13 <vtrcarvalho.13@gmail.com>
2020-03-07 15:15:48 -0300

    correao header

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,12 @@
 
 </body>
 
-<script src="js/jquery.min.js" type="text/javascript"></script>
-<script src="js/jquery.mask.js" type="text/javascript"></script>
-<script src="js/mask.js"></script>
-<script src="js/functions.js"></script>
+<script src="/congresso_direito_fvc/js/jquery.min.js" type="text/javascript"></script>
+<script src="/congresso_direito_fvc/js/jquery.mask.js" type="text/javascript"></script>
+<script src="/congresso_direito_fvc/js/mask.js"></script>
+<script src="/congresso_direito_fvc/js/functions.js"></script>
 
-<script src="js/bootstrap.min.js"></script>
+<script src="/congresso_direito_fvc/js/bootstrap.min.js"></script>
 
 
 </html>
--- includes/header.php
+++ includes/header.php
@@ -4,19 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscrição - Congresso de Direito da FVC</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="/congresso_direito_fvc/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/congresso_direito_fvc/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" type="text/css" href="/congresso_direito_fvc/css/style.css">
+    <link rel="shortcut icon" href="/congresso_direito_fvc/img/favicon.ico" type="image/x-icon">
 </head>
 <body class="bg-dark">
 <main>
     <nav class="navbar">
         <div class="container">
             <a class="navbar-brand" href="index.php">
-                <img src="img/fvclogo.png"  class="d-inline-block align-top" height="50" alt="">
+                <img src="/congresso_direito_fvc/img/fvclogo.png"  class="d-inline-block align-top" height="50" alt="">
             </a>
-            <a href="login.php" class="btn btn-fvc my-2 my-sm-0" type="submit">Acesso restrito</a>
+            <a href="/congresso_direito_fvc/login.php" class="btn btn-fvc my-2 my-sm-0" type="submit">Acesso restrito</a>
         </div>
     </nav>
     
