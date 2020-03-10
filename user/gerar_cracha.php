247a5b0cfe0805b44e01b81d112f444cafa964b2
victordev13 <vtrcarvalho.13@gmail.com>
2020-03-10 10:49:16 -0300

    gerar-cracha arquivos base

--- user/gerar_cracha.php
+++ user/gerar_cracha.php
@@ -3,24 +3,19 @@ require_once 'checkLogin.php';
 require_once 'src/dompdf/autoload.inc.php';
 require_once '../user_functions.php';
 
+
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
+$dompdf->set_base_path('src/');
 
 $nome = "nome";
 $categoria = "categoria";
 
-$html = "
-    <link type='text/css' href='pdf.css' rel='stylesheet'/>
-    <div class='cracha' style='background-image: url('src/model.png');
-    width: 200px;'>
-        <div class='label'>".$nome."</div>
-        <div class='label'>".$categoria."</div>
-        <div class='codigoDeBarra'>".fbarcode(getCPF())."</div>
-    </div>
-";
+$html = file_get_contents("src/cracha.html");
+
 $dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation
