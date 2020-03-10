404b7dc58071138e92bce735a0317d45b28bac88
Vinicius <vnunes13@gmail.com>
2020-03-10 17:06:00 -0300

    Check Login

--- user/gerador.php
+++ user/gerador.php
@@ -3,6 +3,8 @@ setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' );
 date_default_timezone_set( 'America/Sao_Paulo' );
 require('gerar_certificado/fpdf/alphapdf.php');
 require_once '../functions.php';
+include('./checkLogin.php');
+
 
 @session_start();
 
