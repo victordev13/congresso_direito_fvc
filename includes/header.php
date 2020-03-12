d5d631a1a4faa96a63d6b2d8027f2be0dd6c3fa4
Vinícius Nunes Marciano <52388312+viniciuscbb@users.noreply.github.com>
2020-03-12 07:31:53 -0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscrição - Congresso de Direito da FVC</title>
+    <title>Inscrição - III SEMINÁRIO JURÍDICO DO CAD-FVC</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
