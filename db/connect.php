2015c2853cdb5e5e0647cd474fdb1259493dd21b
allas-dorte <allas-dorte@hotmail.com>
2020-03-12 20:25:56 -0300

    Responsive

--- db/connect.php
+++ db/connect.php
@@ -1,13 +1,7 @@
 <?php
 $host="localhost"; 
-<<<<<<< HEAD
 $username="root"; 
 $password="102030"; 
 $db_name="congresso";
-=======
-$username="ivcbr_eventos"; 
-$password="r9cAYc5U%03O^gYELsnm"; 
-$db_name="ivcbr_eventos";
->>>>>>> e88e24cca4469fc9b5a1855f493c8b333eee586f
 
 ?>
